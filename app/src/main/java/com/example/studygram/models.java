4d8d088b8c6eee616464e061300fcd5e6faece7f
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-12 00:50:38 +0200

    AddModels

---
+++ app/src/main/java/com/example/studygram/models.java
@@ -0,0 +1,15 @@
+package com.example.studygram;
+
+public class models {
+    private String title;
+    private String subject:
+    private String description;
+
+    public post ()
+
+        public post (private String title;private String subject:private String description;) {
+         this.title= title;  //Attribute der Klasse Post werden überschrieben
+         this.subject= subject;
+         this.description= description;
+    }
+}
