12b594a48262885ae9afc05f5e7592ae19f07717
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-12 00:51:59 +0200

    FixMistakesInPostClass

--- app/src/main/java/com/example/studygram/models.java
+++ app/src/main/java/com/example/studygram/models.java
@@ -7,7 +7,7 @@ public class models {
 
     public post ()
 
-        public post (private String title;private String subject:private String description;) {
+        public post (String title;String subject ;String description;) {
          this.title= title;  //Attribute der Klasse Post werden überschrieben
          this.subject= subject;
          this.description= description;
