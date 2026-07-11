ea8e33fbbd099da9b8819c6334cb888e15b30729
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-12 01:17:39 +0200

    AddPostXmlFile

---
+++ app/src/main/java/models/Post.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Post {
+    private String title;
+    private String subject;
+    private String description;
+
+    public Post() { //leerer Konstruktor für Firebase
+
+    }
+
+        public Post(String title, String subject , String description) {
+         this.title= title;  //Attribute der Klasse Post werden überschrieben
+         this.subject= subject;
+         this.description= description;
+    }
+    // Getter
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setter
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+}
