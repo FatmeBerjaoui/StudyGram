cc9679b76884da052c9de08be0564f6cbb140c06
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-13 22:24:54 +0200

    FixForYouFragemnt

--- app/src/main/java/com/example/studygram/MainActivity.java
+++ app/src/main/java/com/example/studygram/MainActivity.java
@@ -47,14 +47,6 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.nav_host_fragment_content_main);
 
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
