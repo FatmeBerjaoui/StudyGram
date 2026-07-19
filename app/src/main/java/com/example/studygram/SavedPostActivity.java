eddd0a582212f7e8d7c139c676ea1f4a6507b192
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-07-19 23:54:40 +0200

    Add extension to SavedPostActivity

--- app/src/main/java/com/example/studygram/SavedPostActivity.java
+++ app/src/main/java/com/example/studygram/SavedPostActivity.java
@@ -15,7 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import java.util.ArrayList;
 import java.util.List;
-public class SavedPostActivity {
+public class SavedPostActivity extends AppCompatActivity {
     private ActivitySavedPostsBinding binding;
 
     @Override
