8ab0dd529ad07eabda13dadc702846bd5e00e1f0
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-07-19 23:38:00 +0200

    Add Saved Post skeleton

--- app/src/main/java/com/example/studygram/SavedPostActivity.java
+++ app/src/main/java/com/example/studygram/SavedPostActivity.java
@@ -1,4 +1,15 @@
 package com.example.studygram;
-
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.studygram.databinding.ActivitySavedPostsBinding;
 public class SavedPostActivity {
+    private ActivitySavedPostsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySavedPostsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
