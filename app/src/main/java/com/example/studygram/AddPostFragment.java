eacf86c32c160d100dd95020fbb6685207aed292
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-20 20:12:53 +0200

    ChangeBinding

--- app/src/main/java/com/example/studygram/AddPostFragment.java
+++ app/src/main/java/com/example/studygram/AddPostFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.studygram.databinding.FragmentSecondBinding;
+import com.example.studygram.databinding.FragmentAddPostBinding;
 
 public class AddPostFragment extends Fragment {
 
