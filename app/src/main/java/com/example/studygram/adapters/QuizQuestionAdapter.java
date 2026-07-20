9934b8a122907278344b3d2d6bc4f521148f1c06
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-20 21:38:04 +0200

    ChangeQuizQuestionAdapter

--- app/src/main/java/com/example/studygram/adapters/QuizQuestionAdapter.java
+++ app/src/main/java/com/example/studygram/adapters/QuizQuestionAdapter.java
@@ -15,18 +15,47 @@ import java.util.ArrayList;
 
 public class QuizQuestionAdapter extends RecyclerView.Adapter<QuizQuestionAdapter.ViewHolder> {
 
-    private String frage;
-    private String antwort;
+    private ArrayList<QuizQuestion> fragen;
 
-    public QuizQuestion(String frage, String antwort) {
-        this.frage = frage;
-        this.antwort = antwort;
+    public QuizQuestionAdapter(ArrayList<QuizQuestion> fragen) {
+        this.fragen = fragen;
     }
 
-    public String getFrage() {
-        return frage;
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_quiz_question, parent, false);
+
+        return new ViewHolder(view);
     }
 
-    public String getAntwort() {
-        return antwort;
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        QuizQuestion frage = fragen.get(position);
+
+        holder.tvFrage.setText(frage.getFrage());
+        holder.tvTyp.setText("Typ: " + frage.getTyp());
+
     }
+
+    @Override
+    public int getItemCount() {
+        return fragen.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvFrage;
+        TextView tvTyp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvFrage = itemView.findViewById(R.id.tvFrage);
+            tvTyp = itemView.findViewById(R.id.tvTyp);
+        }
+    }
+}
