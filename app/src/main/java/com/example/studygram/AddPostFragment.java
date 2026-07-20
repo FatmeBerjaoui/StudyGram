7a766d328a3c718666541de26c675ae3ce48c794
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-20 21:26:01 +0200

    ImportAlertDialog

--- app/src/main/java/com/example/studygram/AddPostFragment.java
+++ app/src/main/java/com/example/studygram/AddPostFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import com.example.studygram.models.QuizQuestion;
