7dac94fe11863da87551c0373697b19afe73ec63
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-07-24 01:31:24 +0200

    change register button in login, Add email verification check before allowing login

--- app/src/main/java/com/example/studygram/LoginActivity.java
+++ app/src/main/java/com/example/studygram/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.studygram;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +23,7 @@ public class LoginActivity extends AppCompatActivity{
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        binding.tvGoToRegister.setPaintFlags(binding.tvGoToRegister.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         mAuth = FirebaseAuth.getInstance();
 
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +45,19 @@ public class LoginActivity extends AppCompatActivity{
     private void loginUser() {
         String email = binding.etEmail.getText().toString();
         String password = binding.etPassword.getText().toString();
+        if (task.isSuccessful()) {
+            FirebaseUser user = mAuth.getCurrentUser();
+
+            if (user != null && user.isEmailVerified()) {
+                goToMainActivity();
+            } else {
+                mAuth.signOut();
+                binding.tvError.setVisibility(View.VISIBLE);
+                binding.tvError.setText("Bitte bestätige zuerst deine Email-Adresse");
+            }
+        } else {
+
+        }
 
         if (email.isEmpty()) {
             binding.etEmail.setError("Email wird benötigt");
