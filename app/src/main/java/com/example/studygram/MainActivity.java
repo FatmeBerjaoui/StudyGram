b9069a0f4e6bc7fbf4bd2ac70a457522c787cffb
stayem24 <s_tayem24@stud.hwr-berlin.de>
2026-07-25 02:52:39 +0200

    Connect BottomNavigationView with NavController, handle Profile as separate Activity

--- app/src/main/java/com/example/studygram/MainActivity.java
+++ app/src/main/java/com/example/studygram/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.studygram;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,8 +8,16 @@ import androidx.activity.EdgeToEdge;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import android.view.View;
+import com.example.studygram.databinding.ActivityMainBinding;
+
 
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,7 +34,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -43,36 +51,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        androidx.fragment.app.FragmentContainerView navHost =
-                findViewById(R.id.nav_host_fragment_content_main);
+        // NavController holen, um zwischen den Fragments zu wechseln
+        FragmentContainerView navHostView = findViewById(R.id.nav_host_fragment_content_main);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_main);
+        NavController navController = navHostFragment.getNavController();
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        return super.onSupportNavigateUp();
+        // Klick auf ein Bottom-Nav-Item wird an den NavController weitergegeben,
+        // außer bei "Profile", das eine eigene Activity ist statt eines Fragments
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.profileAction) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                return true;
+            } else {
+                return NavigationUI.onNavDestinationSelected(item, navController);
+            }
+        });
     }
 }
