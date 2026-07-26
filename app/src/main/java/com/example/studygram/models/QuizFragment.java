6a67f33eeadec96237366b2b74a8f6cc1b21e257
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-26 23:51:10 +0200

    AddQuizGame

--- app/src/main/java/com/example/studygram/models/QuizFragment.java
+++ app/src/main/java/com/example/studygram/models/QuizFragment.java
@@ -1,4 +1,45 @@
-package com.example.studygram.models;
+public class QuizFragment extends Fragment {
 
-public class QuizFragment {
+    private FragmentQuizBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentQuizBinding.inflate(inflater, container, false);
+
+        binding.btnLikedQuiz.setOnClickListener(v -> {
+
+            Toast.makeText(getContext(),
+                    "Liked Quiz",
+                    Toast.LENGTH_SHORT).show();
+
+        });
+
+        binding.btnSavedQuiz.setOnClickListener(v -> {
+
+            Toast.makeText(getContext(),
+                    "Saved Quiz",
+                    Toast.LENGTH_SHORT).show();
+
+        });
+
+        binding.btnWrongQuiz.setOnClickListener(v -> {
+
+            Toast.makeText(getContext(),
+                    "Wrong Questions Quiz",
+                    Toast.LENGTH_SHORT).show();
+
+        });
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
 }
