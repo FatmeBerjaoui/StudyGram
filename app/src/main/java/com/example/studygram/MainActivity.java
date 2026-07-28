67b84750d3381d451947fdcc46699facae6cc220
Fatme Berjaoui <s.berjaoui24@hwr-berlin.de>
2026-07-28 22:27:02 +0200

    FixScrollView

--- app/src/main/java/com/example/studygram/MainActivity.java
+++ app/src/main/java/com/example/studygram/MainActivity.java
@@ -44,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
-            //Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-           // v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            //return insets;
-        //});
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
 
         // NavController holen, um zwischen den Fragments zu wechseln
